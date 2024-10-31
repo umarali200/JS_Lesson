c1ad3d145d33606dc21c314e0f324c06e6fc0c62
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-10-31 17:19:21 +0500

    uzgarish

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// let firstName=prompt("Enter your first name");
+// let lastName=prompt("Enter your last name");
+// let confirmation=confirm("18 dan kattamisan?"); 
+// const check=confirmation ? "Siz 18 dan katta":"Siz 18 dan kichik";
+// console.log(firstName , lastName, check , confirmation);
+
