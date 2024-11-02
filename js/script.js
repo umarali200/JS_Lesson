17d18189dd61294b462527d4a58ca391e4ed35d4
Umarali <umaralikomilov4@gmail.com>
2024-11-02 19:46:34 +0500

    umarali komilov

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // let firstName=prompt("Enter your first name");
 // let lastName=prompt("Enter your last name");
-// let confirmation=confirm("18 dan kattamisan?"); 
+// let confirmation=confirm("18 dan kattamisan?");
 // const check=confirmation ? "Siz 18 dan katta":"Siz 18 dan kichik";
 // console.log(firstName , lastName, check , confirmation);
-// console.log("Hello World"); 
+// console.log("Hello World");
 
 // let name="Sardorbek";
 
@@ -17,7 +17,7 @@
 
 // console.log(check);
 
-//let 
+//let
 // let a=15;
 // a=16;
 // a=52;
@@ -26,3 +26,24 @@
 // a=16;//hosting
 // let a=15;
 // console.log(a);
+
+
+
+// // Komilov Umarali
+// let adhamOgirlik = 78,
+//   adghamBuy = 1.69,
+//   sardorOgirlik = 95,
+//   sardorBuy = 1.76;
+// let adhamBMI = adhamOgirlik / adghamBuy;
+// let sardorBMI = sardorOgirlik / sardorBuy;
+// let BMI1 = adhamBMI < sardorBMI;
+// console.log(BMI1);
+
+// let adhamOgirlik1 = 85,
+//   adghamBuy1 = 1.76,
+//   sardorOgirlik1 = 95,
+//   sardorBuy1 = 1.88;
+// let adhamBMI1 = adhamOgirlik1 / adghamBuy1;
+// let sardorBMI1 = sardorOgirlik1 / sardorBuy1;
+// let BMI2 = adhamBMI1 < sardorBMI1;
+// console.log(BMI2);
