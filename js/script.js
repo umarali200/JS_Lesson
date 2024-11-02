946f6f4a5abdc80da350e583178bc3eeee6b17e4
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-11-02 14:23:34 +0500

    asdasdasdasd

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@
 // let confirmation=confirm("18 dan kattamisan?"); 
 // const check=confirmation ? "Siz 18 dan katta":"Siz 18 dan kichik";
 // console.log(firstName , lastName, check , confirmation);
+// console.log("Hello World"); 
 
+// let name="Sardorbek";
+
+// console.log(name);
+
+
+
+// let a="15";
+// let b=5;
+// const check=(a+b);
+
+// console.log(check);
